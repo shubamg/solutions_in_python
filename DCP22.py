d7b5f9527e7c824d9e0bf4451b828e0984fc0ec9
Shubham Gupta <shubham180695.sg@gmail.com>
2019-02-10 10:35:43 -0800

    Add N^2 solution

--- DCP22.py
+++ DCP22.py
@@ -27,6 +27,28 @@ def g(words, sentence):
     return res
 
 
-print(g(['quick', 'brown', 'the', 'fox'], "thequickbrownfox"))
-print(g(['bed', 'bath', 'and', 'beyond'], "bedbathandbeyond"))
-print(g(['a', 'bcd' 'e'], "bceaebcad"))
+def g_optimal(words, sentence):
+    longest_word_length = max(map(len, words))
+    end_index_to_last_word_of_decomposition = {-1: ''}
+    for curr_index in range(len(sentence)):
+        for end_index in end_index_to_last_word_of_decomposition:
+            if (curr_index - end_index) <= longest_word_length and sentence[end_index+1:curr_index+1] in words:
+                end_index_to_last_word_of_decomposition[curr_index] = sentence[end_index+1:curr_index+1]
+                break
+    if (len(sentence)-1) not in end_index_to_last_word_of_decomposition:
+        return [False, []]
+
+    decomposition = []
+    curr_index = len(sentence)-1
+    while curr_index >= 0:
+        word = end_index_to_last_word_of_decomposition[curr_index]
+        curr_index -= len(word)
+        decomposition.append(word)
+
+    decomposition.reverse()
+    return [True, decomposition]
+
+
+print(g_optimal(['quick', 'brown', 'the', 'fox'], "thequickbrownfox"))
+print(g_optimal(['bed', 'bath', 'and', 'beyond'], "bedbathandbeyond"))
+print(g_optimal(['a', 'bcd' 'e'], "bceaebcad"))
